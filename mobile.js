400ff3d0930216a965cd50b80f44ec81a975cef1
senorhuncho <jasonfomenky@gmail.com>
2024-11-12 12:16:03 +0100

    changes

--- mobile.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuToggle = document.getElementById('menu-toggle');
-  const menu = document.querySelector('.menu');
-
-  menuToggle.addEventListener('click', () => {
-      menuToggle.classList.toggle('active');
-      menu.classList.toggle('active');
-  });
-});
